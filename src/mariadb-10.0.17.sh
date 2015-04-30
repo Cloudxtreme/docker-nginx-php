5de7aca04d14deb3faa88f8405597487d72985fe
黑崎時也 <kurotokiya@users.noreply.github.com>
2015-04-30 12:55:38 +0800

    Fix a bug

--- src/mariadb-10.0.17.sh
+++ src/mariadb-10.0.17.sh
@@ -1,3 +1,5 @@
+dbrootpwd="123456"
+
 DOWN_ADDR=https://downloads.mariadb.org/f
 [ -d "/lib64" ] && { SYS_BIT_a=x86_64;SYS_BIT_b=x86_64; } || { SYS_BIT_a=x86;SYS_BIT_b=i686; }
 LIBC_VERSION=`getconf -a | grep GNU_LIBC_VERSION | awk '{print $NF}'`
